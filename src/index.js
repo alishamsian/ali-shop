c0554529f9d04609683946e2a9895026f0e1044e
Ali Shamsian <alishamsian504@gmail.com>
2022-05-01 00:19:50 +0430

    Sass preparation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import reportWebVitals from './reportWebVitals';
 
 
 import "./assets/boxicons-2.0.7/css/boxicons.min.css"
+import "./sass/index.scss"
+
 import Layout from './components/Layout';
 
 

f856baad78d0dce887465f6a6673b0cfb02b63fa
Ali Shamsian <alishamsian504@gmail.com>
2022-05-05 20:48:21 +0430

    add policy-card

---
+++ src/assets/fake-data/policy.js
@@ -0,0 +1,23 @@
+const policy =[
+    {
+        name: 'Policy 1',
+        description: 'Policy 1 description',
+        icon: "bx bx-shopping-bag",
+    },
+    {
+        name: 'Policy 2',
+        description: 'Policy 2 description',
+        icon: "bx bx-credit-card",
+    },
+    {
+        name: 'Policy 3',
+        description: 'Policy 3 description',
+        icon: "bx bx-diamond",
+    },
+    {
+        name: 'Policy 4',
+        description: 'Policy 4 description',
+        icon: "bx bx-donate-heart",
+    }
+] 
+export default policy;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import Hamlet from '../components/Hamlet'
-import HeaderBaner from '../components/HeaderBaner'
-import Section, { SectionBody } from '../components/Section'
-
+import React from "react";
+import policy from "../assets/fake-data/policy";
+import Hamlet from "../components/Hamlet";
+import HeaderBaner from "../components/HeaderBaner";
+import PolicyCard from "../components/PolicyCard";
+import Section, { SectionBody } from "../components/Section";
+import Grid from "../components/Grid";
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <Hamlet title="Home">
-      <HeaderBaner
-        color="blue"
-      />
+      <HeaderBaner color="blue" />
 
       <Section>
         <SectionBody>
-
+          <Grid col={4} mdCol={2} smCol={1} gap={20}>
+            {policy.map((item, index) => (
+              <Link to="/policy">
+                <PolicyCard
+                  key={index}
+                  name={item.name}
+                  description={item.description}
+                  icon={item.icon}
+                />
+              </Link>
+            ))}
+          </Grid>
         </SectionBody>
       </Section>
-
     </Hamlet>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;

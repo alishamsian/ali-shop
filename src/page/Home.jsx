ce149d7575b9f514861db05465d02e8d8e47c3b9
Ali Shamsian <alishamsian504@gmail.com>
2022-05-06 02:52:22 +0430

    add product card and numberWitthCommas

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -3,21 +3,25 @@ import policy from "../assets/fake-data/policy";
 import Hamlet from "../components/Hamlet";
 import HeaderBaner from "../components/HeaderBaner";
 import PolicyCard from "../components/PolicyCard";
-import Section, { SectionBody } from "../components/Section";
+import Section, { SectionBody, SectionTitle } from "../components/Section";
 import Grid from "../components/Grid";
 import { Link } from "react-router-dom";
+ 
+import productData from "../assets/fake-data/products";
+import ProductCard from "../components/ProductCard";
+
 const Home = () => {
   return (
     <Hamlet title="Home">
       <HeaderBaner color="blue" />
 
+      {/* policy section */}
       <Section>
         <SectionBody>
           <Grid col={4} mdCol={2} smCol={1} gap={20}>
             {policy.map((item, index) => (
-              <Link to="/policy">
+              <Link to="/policy" key={index}>
                 <PolicyCard
-                  key={index}
                   name={item.name}
                   description={item.description}
                   icon={item.icon}
@@ -27,6 +31,29 @@ const Home = () => {
           </Grid>
         </SectionBody>
       </Section>
+      {/* end policy section */}
+
+      {/* best section */}
+
+      <Section>
+        <SectionTitle>Best products</SectionTitle>
+        <SectionBody>
+          <Grid col={4} mdCol={2} smCol={1} gap={20}>
+            {productData.getProducts(4).map((item, index) => (
+              <ProductCard
+                key={index}
+                img01={item.image01}
+                img02={item.image02}
+                name={item.title}
+                price={Number(item.price)}
+                slug={item.slug}
+              />
+            ))}
+          </Grid>
+        </SectionBody>
+      </Section>
+
+      {/* end best section */}
     </Hamlet>
   );
 };

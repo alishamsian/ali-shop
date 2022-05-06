e7b343f8136463eed509eb13cf8c32e3f8fab9b8
Ali Shamsian <alishamsian504@gmail.com>
2022-05-06 14:19:41 +0430

    response filter

--- src/page/Catalog.jsx
+++ src/page/Catalog.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, {useRef} from "react";
 import Hamlet from "../components/Hamlet";
 import Grid from "../components/Grid";
 import productData from "../assets/fake-data/products";
 import ProductCard from "../components/ProductCard";
 import category from "../assets/fake-data/category";
+import Button from "../components/Button";
 
 const Catalog = () => {
+
+  const filterRef = useRef(null)
+
+  const showHideFilter = () => filterRef.current.classList.toggle("active")
+
   return (
     <Hamlet title="Catalog">
       <div className="catalog">
-        <div className="catalog__filter">
+        <div className="catalog__filter " ref={filterRef}>
+          <div className="catalog__filter__close" onClick={()=> showHideFilter()}>
+            <i className="bx bx-left-arrow-alt"></i>
+          </div>
           <div className="catalog__filter__widget">
             <div className="catalog__filter__widget__title">filter</div>
             <div className="catalog__filter__widget__content">
@@ -35,6 +44,9 @@ const Catalog = () => {
             </div>
           </div>
         </div>
+          <div className="catalog__filter__toggle">
+            <Button size="sm" onClick={()=>showHideFilter()}>filter</Button>
+          </div>
         <div className="catalog__content">
           <Grid col={3} mdCol={2} smCol={1} gap={20}>
             {productData.getAllProducts().map((item, index) => (
